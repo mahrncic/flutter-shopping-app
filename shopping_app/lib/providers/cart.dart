0142f6c07a9af2f1bd3e2099d7206d4f5ff851a0
Marko Hrncic <marko.hrncic@kogitas.com>
2021-12-04 15:37:21 +0100

    Implementation of base classes for cart and cart items.

---
+++ shopping_app/lib/providers/cart.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_complete_guide/models/cart_item.dart';
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items;
+
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  void addItem(String productId, double price, String title) {
+    if (_items.containsKey(productId)) {
+      _items.update(
+          productId,
+          (existingCartItem) => CartItem(
+                id: existingCartItem.id,
+                title: existingCartItem.title,
+                price: existingCartItem.price,
+                quantity: existingCartItem.quantity + 1,
+              ));
+    } else {
+      _items.putIfAbsent(
+        productId,
+        () => CartItem(
+            id: DateTime.now().toString(),
+            title: title,
+            price: price,
+            quantity: 1),
+      );
+    }
+  }
+}
