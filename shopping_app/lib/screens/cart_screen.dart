474866ce42345beeff7dc9cff8b18022e7aa1d56
Marko Hrncic <marko.hrncic@kogitas.com>
2021-12-06 21:06:26 +0100

    Added functionality for performing Order operation.

--- shopping_app/lib/screens/cart_screen.dart
+++ shopping_app/lib/screens/cart_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_complete_guide/providers/cart.dart';
+import 'package:flutter_complete_guide/providers/orders.dart';
 import 'package:flutter_complete_guide/widgets/cart_item.dart';
 import 'package:provider/provider.dart';
 
@@ -40,7 +41,16 @@ class CartScreen extends StatelessWidget {
                     backgroundColor: Theme.of(context).primaryColor,
                   ),
                   FlatButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Provider.of<Orders>(
+                        context,
+                        listen: false,
+                      ).addOrder(
+                        cart.items.values.toList(),
+                        cart.totalAmount,
+                      );
+                      cart.clearCart();
+                    },
                     textColor: Theme.of(context).primaryColor,
                     child: Text('ORDER NOW'),
                   )
