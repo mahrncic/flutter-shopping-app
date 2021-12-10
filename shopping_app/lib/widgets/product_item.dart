ce35d8e3ef324859bc2f916d6401c5b9845f8184
Marko Hrncic <marko.hrncic@kogitas.com>
2021-12-10 19:27:53 +0100

    Implementation of snackbar and undo functionality when adding new item into the cart.

--- shopping_app/lib/widgets/product_item.dart
+++ shopping_app/lib/widgets/product_item.dart
@@ -49,6 +49,21 @@ class ProductItem extends StatelessWidget {
                 product.price,
                 product.title,
               );
+              Scaffold.of(context).hideCurrentSnackBar();
+              Scaffold.of(context).showSnackBar(SnackBar(
+                content: Text(
+                  'Added ${product.title} to cart',
+                ),
+                duration: Duration(
+                  seconds: 2,
+                ),
+                action: SnackBarAction(
+                  label: 'UNDO',
+                  onPressed: () {
+                    cart.removeSingleItem(product.id);
+                  },
+                ),
+              ));
             },
             color: Theme.of(context).accentColor,
           ),
