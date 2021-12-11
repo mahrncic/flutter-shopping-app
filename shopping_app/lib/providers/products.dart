03a1085ebe6f2102ca45444cb1aba0d11a86447b
Marko Hrncic <marko.hrncic@kogitas.com>
2021-12-11 12:05:53 +0100

    Implementation of id assignment that is fetched from the Firebase server.

--- shopping_app/lib/providers/products.dart
+++ shopping_app/lib/providers/products.dart
@@ -60,7 +60,8 @@ class Products with ChangeNotifier {
     final url = Uri.parse(
         'https://flutter-shopping-app-8ca69-default-rtdb.firebaseio.com/products.json');
 
-    http.post(
+    http
+        .post(
       url,
       body: json.encode({
         'title': product.title,
@@ -69,19 +70,20 @@ class Products with ChangeNotifier {
         'price': product.price,
         'isFavorite': product.isFavorite,
       }),
-    );
+    )
+        .then((response) {
+      final newProduct = Product(
+        title: product.title,
+        description: product.description,
+        imageUrl: product.imageUrl,
+        price: product.price,
+        isFavorite: product.isFavorite,
+        id: json.decode(response.body)['name'],
+      );
 
-    final newProduct = Product(
-      title: product.title,
-      description: product.description,
-      imageUrl: product.imageUrl,
-      price: product.price,
-      isFavorite: product.isFavorite,
-      id: DateTime.now().toString(),
-    );
-
-    _items.add(newProduct);
-    notifyListeners();
+      _items.add(newProduct);
+      notifyListeners();
+    });
   }
 
   void updateProduct(String id, Product editedProduct) {
