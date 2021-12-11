88499fde6d1316d2ad9ef5125c35c36fc3d7513e
Marko Hrncic <marko.hrncic@kogitas.com>
2021-12-11 12:54:09 +0100

    Implementation of error handling for 'Add Product' functionality.

--- shopping_app/lib/providers/products.dart
+++ shopping_app/lib/providers/products.dart
@@ -83,6 +83,8 @@ class Products with ChangeNotifier {
 
       _items.add(newProduct);
       notifyListeners();
+    }).catchError((error) {
+      throw error;
     });
   }
 
--- shopping_app/lib/screens/edit_product_screen.dart
+++ shopping_app/lib/screens/edit_product_screen.dart
@@ -95,7 +95,23 @@ class _EditProductScreenState extends State<EditProductScreen> {
     } else {
       Provider.of<Products>(context, listen: false)
           .addProduct(_editedProduct)
-          .then((_) {
+          .catchError((error) {
+        return showDialog<Null>(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: const Text('An error occurred!'),
+            content: Text('Something went wrong.'),
+            actions: [
+              FlatButton(
+                onPressed: () {
+                  Navigator.of(ctx).pop();
+                },
+                child: Text('Okay'),
+              ),
+            ],
+          ),
+        );
+      }).then((_) {
         setState(() {
           _isLoading = false;
         });
