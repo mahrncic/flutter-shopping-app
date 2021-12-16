a8c0da5cd7fd2d9a6b6c57f88e0d267ca145c6d2
Marko Hrncic <marko.hrncic@kogitas.com>
2021-12-16 21:18:52 +0100

    Implementation of error handling with Alert Dialog - Login functionality.

--- shopping_app/lib/providers/auth.dart
+++ shopping_app/lib/providers/auth.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/widgets.dart';
+import 'package:flutter_complete_guide/models/exceptions/http_exception.dart';
 import 'package:http/http.dart' as http;
 
 class Auth with ChangeNotifier {
@@ -13,16 +14,22 @@ class Auth with ChangeNotifier {
     final url = Uri.parse(
         'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyDnI3jIGH6Y-_ZooN8stEo32STsjgjFVtY');
 
-    final response = await http.post(
-      url,
-      body: json.encode({
-        'email': email,
-        'password': password,
-        'returnSecureToken': true,
-      }),
-    );
-
-    print(json.decode(response.body));
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode({
+          'email': email,
+          'password': password,
+          'returnSecureToken': true,
+        }),
+      );
+      final responseData = json.decode(response.body);
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+    } catch (error) {
+      throw error;
+    }
   }
 
   Future<void> signup(String email, String password) async {
