7668ea8af5fedc1cc74d2741d695185a199156cb
Marko Hrncic <marko.hrncic@kogitas.com>
2021-12-18 18:47:04 +0100

    Adjusting Animation functionality with AnimatedBuilder widget.

--- shopping_app/lib/screens/auth_screen.dart
+++ shopping_app/lib/screens/auth_screen.dart
@@ -119,8 +119,6 @@ class _AuthCardState extends State<AuthCard>
       parent: _animationController,
       curve: Curves.fastOutSlowIn,
     ));
-
-    _heightAnimation.addListener(() => setState(() {}));
   }
 
   @override
@@ -214,11 +212,15 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: Container(
-        height: _heightAnimation.value.height,
-        constraints: BoxConstraints(minHeight: _heightAnimation.value.height),
-        width: deviceSize.width * 0.75,
-        padding: EdgeInsets.all(16.0),
+      child: AnimatedBuilder(
+        animation: _heightAnimation,
+        builder: (ctx, widgetChild) => Container(
+          height: _heightAnimation.value.height,
+          constraints: BoxConstraints(minHeight: _heightAnimation.value.height),
+          width: deviceSize.width * 0.75,
+          padding: EdgeInsets.all(16.0),
+          child: widgetChild,
+        ),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
@@ -231,7 +233,6 @@ class _AuthCardState extends State<AuthCard>
                     if (value.isEmpty || !value.contains('@')) {
                       return 'Invalid email!';
                     }
-                    return null;
                     return null;
                   },
                   onSaved: (value) {
